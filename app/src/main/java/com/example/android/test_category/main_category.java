e8e5024f8a911be282619427350148c3c53066d3
qaysmaher <qaysmaher971@gmail.com>
2019-04-12 22:12:06 +0300

    get latitude and longitude

--- app/src/main/java/com/example/android/test_category/main_category.java
+++ app/src/main/java/com/example/android/test_category/main_category.java
@@ -3,53 +3,66 @@ package com.example.android.test_category;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.text.DecimalFormat;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class main_category extends AppCompatActivity {
 
-    private FusedLocationProviderClient clint;
+    private FusedLocationProviderClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_category);
 
-        clint = LocationServices.getFusedLocationProviderClient(this);
+        Button get_location = findViewById(R.id.get);
+        final TextView put_location = findViewById(R.id.put);
+        client = LocationServices.getFusedLocationProviderClient(this);
 
-        Button get = findViewById(R.id.get);
+        requsetPermissin();
 
-        get.setOnClickListener(new View.OnClickListener() {
+        get_location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(main_category.this, ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(main_category.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(main_category.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     return;
                 }
-                clint.getLastLocation().addOnSuccessListener(main_category.this, new OnSuccessListener<Location>() {
+                client.getLastLocation().addOnSuccessListener(main_category.this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         if(location!=null){
-                            TextView put=findViewById(R.id.put);
-                            put.setText(location.toString());
+                            Double getLatitude=location.getLatitude();
+                            Double getLongitude=location.getLongitude();
 
+                            put_location.setText(getLatitude.toString()+"   "+getLongitude.toString());
                         }
+
+
+
                     }
                 });
             }
         });
+
     }
 
-    private void requestpermissin(){
+    private void requsetPermissin(){
         ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION},1);
-        //////////////////////////////////////
     }
+
 }
